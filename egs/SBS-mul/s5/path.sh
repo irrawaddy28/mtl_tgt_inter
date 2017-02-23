5c5cd22f96f7c0f678ce776719430bbb216cd582
irrawaddy28 <irrawaddy28@users.noreply.github.com>
2017-02-22 21:39:24 -0600

    set host dependent paths

--- egs/SBS-mul/s5/path.sh
+++ egs/SBS-mul/s5/path.sh
@@ -1,4 +1,8 @@
-export KALDI_ROOT=/ws/ifp-48_1/hasegawa/amitdas/work/SBS-kaldi-2015
+if [ `hostname` == "ifp-48" ]; then
+  export KALDI_ROOT=/ws/ifp-48_1/hasegawa/amitdas/work/SBS-kaldi-2015
+elif  [ `hostname` == "ifp-04" ]; then
+  export KALDI_ROOT=/ws/ifp-04_2/hasegawa/amitdas/work/SBS-kaldi-2015
+fi
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/sph2pipe_v2.5/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/irstlm/bin/:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/kwsbin:$PWD:$PATH:/export/ws15-pt-data/python-3.4.3/bin
 export LC_ALL=C
 export IRSTLM=$KALDI_ROOT/tools/irstlm
